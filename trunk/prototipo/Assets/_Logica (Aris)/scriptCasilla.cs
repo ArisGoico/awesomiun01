41219a85822026adbbec015c4095005f7686c644
aris.goico@gmail.com <aris.goico@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-10-26 11:30:36 +0000

    Algunos añadidos más de organización para facilitar el trabajo.

---
+++ trunk/prototipo/Assets/_Logica (Aris)/scriptCasilla.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptCasilla : MonoBehaviour {
+
+	//Este script solo contiene variables publicas para usar por el resto de scripts
+	public bool color1		= false;
+	public bool color2		= false;
+	public bool color3		= false;
+	
+}
