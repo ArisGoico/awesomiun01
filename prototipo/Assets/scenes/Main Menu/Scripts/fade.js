b37392c97864ac34a64e317dd7d55908b36ab5bc
blackMafin@gmail.com <blackMafin@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-10-26 23:41:14 +0000

    Base para el Menu

---
+++ prototipo/Assets/scenes/Main Menu/Scripts/fade.js
@@ -0,0 +1,36 @@
+﻿public var fadeOutTexture: Texture2D;
+public var fadeSpeed = 2.0;
+public var fadeTime = 2.0;
+public var alphaWait : boolean = true;
+
+private var alpha = 1.0;
+private var fadeDir = -1;
+
+function Start () {
+	alpha=1;
+	fadeIn();
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+	if(alphaWait == false) {
+	alpha += fadeDir * fadeSpeed * Time.deltaTime;
+	}
+	alpha = Mathf.Clamp01(alpha);  
+	
+	GUI.color.a = alpha;
+	GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+}
+
+function fadeIn(){
+    yield WaitForSeconds(fadeTime);
+    alphaWait = false;
+    fadeDir = -1;  
+}
+  
+function fadeOut(){
+    fadeDir = 1; 
+}
