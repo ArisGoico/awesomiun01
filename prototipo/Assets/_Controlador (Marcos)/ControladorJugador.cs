c01534f65d4e526d05a990e5ba8c9855d32a607a
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-10-27 23:12:04 +0000

    Controlador del jugador mas o menos funcionando. Problema con raycast que colisiona con un layer diferente al que debería.

---
+++ prototipo/Assets/_Controlador (Marcos)/ControladorJugador.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorJugador : MonoBehaviour
+{
+	private float lastPresTime = 0.0f;
+	
+	public void Update()
+	{
+		if(Time.time > lastPresTime + 0.25)
+		{
+			if(Input.GetButton("UP"))
+			{
+				lastPresTime = Time.time;				
+				if(Utilidades.hayCasilla(transform.position.x,transform.position.z+1))
+					transform.Translate(Vector3.forward,Space.World);
+			}
+			else if(Input.GetButton("LEFT"))
+			{
+				lastPresTime = Time.time;				
+				if(Utilidades.hayCasilla(transform.position.x-1,transform.position.z))
+					transform.Translate(Vector3.left,Space.World);
+			}
+			else if(Input.GetButton("RIGHT"))
+			{
+				lastPresTime = Time.time;				
+				if(Utilidades.hayCasilla(transform.position.x+1,transform.position.z))
+					transform.Translate(Vector3.right,Space.World);
+			}
+			else if(Input.GetButton("DOWN"))
+			{
+				lastPresTime = Time.time;				
+				if(Utilidades.hayCasilla(transform.position.x,transform.position.z-1))
+					transform.Translate(Vector3.back,Space.World);
+			}	
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+	    {
+	        if(Camera.main.fieldOfView > 30)
+				Camera.main.fieldOfView -= 3;
+	    }
+	    if (Input.GetAxis("Mouse ScrollWheel") < 0)
+	    {
+	        if(Camera.main.fieldOfView < 90)
+				Camera.main.fieldOfView += 3;
+	    }
+	}
+	
+	
+}
