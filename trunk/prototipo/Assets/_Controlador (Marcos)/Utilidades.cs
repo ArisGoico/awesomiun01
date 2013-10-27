ff1906ee8fd6908ad064f1d2ef901feb14fbc713
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-10-27 23:12:04 +0000

    Controlador del jugador mas o menos funcionando. Problema con raycast que colisiona con un layer diferente al que debería.

---
+++ trunk/prototipo/Assets/_Controlador (Marcos)/Utilidades.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utilidades 
+{
+	public static GameObject dameCasilla(float x, float z)
+	{
+		RaycastHit hit;
+		GameObject obj = null;
+		int layerMask = 1 << 8;
+		if(Physics.Raycast(new Vector3(x,1,z), Vector3.down, out hit, Mathf.Infinity, layerMask))
+		{
+			obj = hit.collider.transform.parent.gameObject;		
+		}		
+		return obj;		
+	}
+	
+	public static bool hayCasilla(float x, float z)
+	{
+		return dameCasilla(x,z) != null;
+	}
+}
