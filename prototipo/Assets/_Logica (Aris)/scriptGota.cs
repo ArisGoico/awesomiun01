3f062a513aff80ecba3c93a4e7e760357724e36d
aris.goico@gmail.com <aris.goico@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-10-30 20:10:58 +0000

    Introducido ritmo y lanzamiento de gotas v1.

---
+++ prototipo/Assets/_Logica (Aris)/scriptGota.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptGota : MonoBehaviour {
+
+	public colorBool colorGota;
+	
+	
+	public void tocarCasilla() {
+		Debug.Log("Tocada la casilla y teñida de " + colorGota + ".");	
+	}
+	
+	public void morir() {
+		GameObject.Destroy(this.transform.parent.gameObject);
+	}
+}
