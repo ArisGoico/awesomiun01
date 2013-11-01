0085c1f58b4f7f74d108a3e4dff2b78559bd9c9c
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-11-01 01:48:45 +0000

    Añadida interacción para quitarle color a las casillas con las teclas 1,2,3 y R,G,B Centrado el jugador en el centro del tablero

--- trunk/prototipo/Assets/_Logica (Aris)/scriptCasilla.cs
+++ trunk/prototipo/Assets/_Logica (Aris)/scriptCasilla.cs
@@ -7,4 +7,29 @@ public class scriptCasilla : MonoBehaviour {
 	//Este script solo contiene variables publicas para usar por el resto de scripts
 	public colorBool color;
 	public int ordenControl;
+	
+	public void aplicarRojo()
+	{
+		color.r = true;		
+	}
+	public void aplicarVerde()
+	{
+		color.g = true;		
+	}
+	public void aplicarAzul()
+	{
+		color.b = true;		
+	}
+	public void quitarRojo()
+	{
+		color.r = false;		
+	}
+	public void quitarVerde()
+	{
+		color.g = false;		
+	}
+	public void quitarAzul()
+	{
+		color.b = false;		
+	}	
 }
