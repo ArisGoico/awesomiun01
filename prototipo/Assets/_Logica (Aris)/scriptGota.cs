8d4b439c0761335053d937dace1a2fdf26ca0d83
blackMafin@gmail.com <blackMafin@gmail.com@77392d72-9339-7676-ff2d-2415c54114b6>
2013-11-02 17:14:33 +0000

    splash!

--- prototipo/Assets/_Logica (Aris)/scriptGota.cs
+++ prototipo/Assets/_Logica (Aris)/scriptGota.cs
@@ -8,16 +8,23 @@ public class scriptGota : MonoBehaviour {
 	public controlCasilla control;
 	public int numCasilla;
 	public LogicaControl scriptPadre;
+	public GameObject splash;
+	private GameObject splashInstance;
 	
 	
 	public void tocarCasilla() {
 		if (scriptPadre == null)
 			scriptPadre = GameObject.FindGameObjectWithTag("script").GetComponent<LogicaControl>();
 		scriptPadre.cambiaColorGota(colorGota, casilla, control, numCasilla);
+		splashInstance = (GameObject)Instantiate(splash, transform.position, splash.transform.rotation);
+		splashInstance.particleSystem.renderer.material = transform.renderer.material;
+		
 //		Debug.Log("Tocada la casilla y teñida de " + colorGota + ".");	
 	}
 	
 	public void morir() {
+		GameObject.Destroy(splashInstance, 2.5f);
 		GameObject.Destroy(this.transform.parent.gameObject);
+		
 	}
 }
